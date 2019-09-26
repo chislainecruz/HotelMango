29dd69e551288c218f04c2bb2c4563d399b7da7b
Chislaine Cruz <chislaine.cruz@gmail.com>
2019-09-25 20:47:03 -0400

    Initial Commit

---
+++ HotelMango/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotelMango
+//
+//  Created by Chislaine Cruz on 9/25/19.
+//  Copyright © 2019 Chislaine Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
